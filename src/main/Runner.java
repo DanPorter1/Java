477b7e46b99fbc7f18f2c75ab7faddd13fc7e849
DanPorter1 <158497242+DanPorter1@users.noreply.github.com>
2026-02-09 16:37:38 +0000

    fixed menu

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -1,12 +1,15 @@
 package main;
 
 import lab2.Grades;
+import lab3.Menu;
 
 public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Grades.results("Daniel", 80);
+		// TODO Condition to run 
+		Menu.lunchQueue();
 	}
 
 }
