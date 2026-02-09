c3ee8321348bb9aeb623e50045cc51226571bb41
DanPorter1 <158497242+DanPorter1@users.noreply.github.com>
2026-02-09 13:36:25 +0000

    loops on grade

--- src/lab2/Program.java
+++ src/lab2/Program.java
@@ -4,18 +4,35 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int grade = 80;
+		String name = "Daniel";
+		results(name, grade);
 		
-		int mark = 55;
 		
-		if (mark > 70) {
-			System.out.println("Distinction");
-		} else if (mark > 60 && mark <= 70) {
-			System.out.println("Marit");
-		} else if (mark > 50 && mark <= 60) {
-			System.out.println("Pass");
-		} else {
-			System.out.println("Fail");
+//		Attempt of arrays
+		String[] names = {"Daniel", "Tom", "Chris", "John" };
+		int[] grades = {45, 60, 80, 68};
+		for (int i = 0; i < names.length; i++) {
+			results(names[i], grades[i]);
 		}
+		
+	}
+		
+	public static String markGrade(int mark) {
+		if (mark > 70) {
+			return"Distinction";
+		} else if (mark > 60 && mark <= 70) {
+			return"Marit";
+		} else if (mark > 50 && mark <= 60) {
+			return"Pass";
+		} else {
+			return"Fail";
+		}
+	}
+	
+	public static void results(String name, int grade) {
+		String result = markGrade(grade);
+		System.out.println(name + " you scored " + grade + " which is a " + result);
 	}
 
 }
